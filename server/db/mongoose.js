cb894f1b0fc5b8b80abb01863f2e210d44cfd16e
Dokkora <thecersolox@gmail.com>
2018-04-07 18:36:26 -0300

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://celso:123piroca@ds231589.mlab.com:31589/interfy', {useMongoClient: true});
+mongoose.connect(process.env.MONGO_STR || 'mongodb://celso:123piroca@ds231589.mlab.com:31589/interfy', {useMongoClient: true});
 
 module.exports = {mongoose};
