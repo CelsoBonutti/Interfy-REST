d23213262921890bdafb7bc51b1877e8ba06f59d
Dokkora <thecersolox@gmail.com>
2018-05-06 18:18:10 -0300

    hehe xd

--- server/server.js
+++ server/server.js
@@ -25,90 +25,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
-
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/todos', (req, res) =>{
-  Todo.find().then((todos)=>{
-    res.send({todos});
-  }, (e) =>{
-    res.status(400).send(e);
-  });
-})
-
-app.get('/todos/:id', (req, res) =>{
-  var id = req.params.id;
-  if(ObjectId.isValid(id)){
-    Todo.findById(id).then((todo)=>{
-      if(!todo){
-        res.status(404).send();
-      }
-      else{
-        res.status(200).send({todo});
-      }
-    }).catch((e)=>{
-        res.status(400).send();
-    })
-  }
-  else{
-    res.status(404).send('ID is not valid.');
-  }
-});
-
-app.delete('/todos/:id', (req, res)=>{
-  var id = req.params.id;
-  if(!ObjectId.isValid(id)){
-    return res.status(404).send();
-  }
-
-  Todo.findByIdAndRemove(id).then((todo) =>{
-    if(!todo){
-      return res.status(404).send();
-    }
-
-    res.send({todo});
-  }).catch((e) =>{
-    res.status(400).send();
-  });
-});
-
-app.patch('/todos/:id', (req, res) =>{
-  var id = req.params.id;
-  var body = _.pick(req.body, ['text', 'completed']);
-
-  if(!ObjectId.isValid(id)){
-    return res.status(404).send();
-  }
-
-  if(_.isBoolean(body.completed) && body.completed){
-    body.completedAt = new Date().getTime();
-  } else{
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
-    .then((todo) =>{
-      if(!todo){
-        res.status(404).send();
-      }
-
-      res.send({todo});
-    }).catch((e) =>{
-      res.status(400).send();
-    })
-})
-
-app.post('/users', (req, res) => {
+app.post('/users/register', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
@@ -123,6 +40,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate,(req, res) =>{
   res.send(req.user);
+})
+
+app.post('/users/login', (req, res)=>{
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) =>{
+    return user.generateAuthToken().then((token) =>{
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) =>{
+    res.status(400).send();
+  })
 })
 
 app.listen(port, () => {
