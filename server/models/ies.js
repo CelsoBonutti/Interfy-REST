7af0b5f68a81f36e4e284be8f051a04929d24e6d
unknown <thecersolox@gmail.com>
2018-06-16 17:38:49 -0300

    Países e detalhes do intercâmbio adicionados

--- server/models/ies.js
+++ server/models/ies.js
@@ -1,57 +1,78 @@
 const mongoose = require('mongoose');
-const validator = require('validator'); 
+const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
-var DiferencialSchema = new mongoose.Schema({
-    descricao: {
-        type: String,
-        required: true
-    },
-    icone: {
-        type: String,
-        validate:{
-            validator: validator.isURL,
-            message: '{VALUE} não é uma URL válida'
-        }
-    } 
-})
-
 var InstituicaoSchema = new mongoose.Schema({
-    nome:{
+    nome: {
         type: String,
         required: true,
         trim: false,
         minlength: 5,
         unique: false,
     },
-    pais:{
+    pais: {
         type: string,
         minlength: 2,
         maxlength: 2,
         required: true
     },
-    cidade:{
+    cidade: {
         type: string,
         maxlength: 20,
         required: true
     },
-    diferenciais:{
+    diferenciais: {
         type: [diferencialSchema]
     },
-    cursos:{
+    cursos: {
         type: [CursoSchema]
     },
-    fotos:{
+    fotos: {
         type: [String],
         required: true,
-    }
+    },
+    diferenciais: [{
+        descricao: {
+            type: String,
+            required: true
+        },
+        icone: {
+            type: String,
+            required: true
+        }
+    }],
+    comentarios: {
+        usuario: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        nota: {
+            type: Number,
+            min: 0,
+            max: 5,
+            required: true
+        },
+        texto: {
+            type: String,
+            required: true
+        }
+    },
+    infraestrutura: [{
+        type: String,
+        required: true
+    }],
+    atividadesExtra: [{
+        type: String,
+        required: true
+    }]
 })
 
-InstituicaoSchema.statics.findByFilter = function(){
+InstituicaoSchema.statics.findByFilter = function () {
     return InstituicaoSchema.findBy(filter);
 }
 
 var Instituicao = mongoose.model('Instituicao', InstituicaoSchema);
 
-module.exports = {Instituicao};
+module.exports = { Instituicao };
--- server/models/intercambios.js
+++ server/models/intercambios.js
@@ -37,14 +37,6 @@ var IntercambioSchema = new mongoose.Schema({
     }
 })
 
-
-IntercambioSchema.methods.numeroSemanas = function(){
-    var dataInicio = moment(this.curso.datasInicio[0]);
-    var dataFim = moment(this.curso.datasInicio[0]);
-    
-    return dataFim.diff(dataInicio, 'weeks');
-}
-
 IntercambioSchema.methods.calcularValor = function() {
     var valor = this.curso.calcularValor();
     extras.foreach(extra =>{
