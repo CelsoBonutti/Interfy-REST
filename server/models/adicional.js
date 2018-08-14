63a4a50822ce906bc78e5ec9e113112777b40ab2
Celso Bonutti <celso.bonuttif@gmail.com>
2018-08-14 00:50:34 -0300

    Alterado o retorno da rota de busca de escolas
    Foi realizada uma alteração na estrutura dos dados que retornam de um GET para /escolas, facilitando a visualização dos dados por parte do frontend.

--- server/models/adicional.js
+++ server/models/adicional.js
@@ -3,51 +3,59 @@ const validator = require('validator');
 
 
 let AdicionalSchema = new mongoose.Schema({
- descricao: {
-    type: String,
-    required: true
+    descricao: {
+        type: String,
+        required: true
     },
     valor: {
-    type: Number,
-    required: true,
-    validate: {
-    validator: validator.isCurrency,
-    message: '{VALUE} não é um valor válido.'
+        type: Number,
+        required: true,
+        validate: {
+            validator: validator.isCurrency,
+            message: '{VALUE} não é um valor válido.'
         }
     },
-instituicao: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Instituicao',
-    required: false,
-    validate:{
-    validator: function(instituicao){
-        let { Instituicao } = require('./escola');  
-        return Instituicao.exists(instituicao);
-                },
-        message: 'Escola inexistente'
-                }
+    instituicao: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Instituicao',
+        required: false,
+        validate: {
+            validator: function (instituicao) {
+                let {
+                    Instituicao
+                } = require('./escola');
+                return Instituicao.exists(instituicao);
             },
-intercambio: {
-    type: mongoose.Schema.Types.ObjectId,
+            message: 'Escola inexistente'
+        }
+    },
+    intercambio: {
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'Intercambio',
         required: false,
-        validate:{
-                validator: function(intercambio){
-                    let { Intercambio } = require('./intercambios');  
-                        return Intercambio.exists(intercambio);
-                    },
-                    message: 'Intercambio inexistente'
-                }
-            }
+        validate: {
+            validator: function (intercambio) {
+                let {
+                    Intercambio
+                } = require('./intercambios');
+                return Intercambio.exists(intercambio);
+            },
+            message: 'Intercambio inexistente'
+        }
+    }
 })
 
-AdicionalSchema.statics.exists = function(id){
+AdicionalSchema.statics.exists = function (id) {
     Adicional = this;
-    return Adicional.count({_id: id}).then((count)=>{
-        return (count>0);
+    return Adicional.count({
+        _id: id
+    }).then((count) => {
+        return (count > 0);
     })
 }
 
 let Adicional = mongoose.model('Adicional', AdicionalSchema);
 
-module.exports = { Adicional };
+module.exports = {
+    Adicional
+};
--- server/routes/escolas.js
+++ server/routes/escolas.js
@@ -7,7 +7,47 @@ const { Instituicao } = require('../models/escola');
 router.get('/', (req, res) => {
     let filter = req.query;
     Instituicao.findByFilter(filter).then((turnos) => {
-        res.status(200).send(turnos);
+
+        intensidades = _.uniq(_.map(turnos, 'intensidade'));
+        turnos = turnos.map(turno =>{
+            intensidadeId = turno.intensidade._id;
+            turno = _.omit(turno, ['intensidade', 'instituicao']);
+            turno.intensidade = intensidadeId;
+            return turno;
+        })
+
+        cursos = _.uniq(_.map(intensidades, 'curso'));
+        intensidades = intensidades.map(intensidade =>{
+            cursoId = intensidade.curso._id;
+            intensidade = _.omit(intensidade, 'curso');
+            intensidade.curso = cursoId;
+            return intensidade;
+        })
+
+        escolas = _.uniq(_.map(cursos, 'instituicao'));
+        cursos = cursos.map(curso =>{
+            instituicaoId = curso.instituicao._id;
+            curso = _.omit(curso, 'instituicao');
+            curso.instituicao = instituicaoId;
+            return curso;
+        })
+
+        intensidades = intensidades.map(intensidade =>{
+            intensidade.turnos = _.filter(turnos, {'intensidade': intensidade._id});
+            return intensidade;
+        })
+
+        cursos = cursos.map(curso =>{
+            curso.intensidades = _.filter(intensidades, {'curso': curso._id});
+            return curso;
+        })
+        
+        escolas = escolas.map(escola =>{
+            escola.cursos = _.filter(cursos, {'instituicao': escola._id});
+            return escola;
+        })
+
+        res.status(200).send(escolas);
     }, (e) => {
         res.status(400).send(e);
     })
